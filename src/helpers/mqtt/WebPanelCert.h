98fe78a379ba8033e290eaaeef42287d42f7f544
Jared Dohrman <jared@dohrman.me>
2026-04-09 15:00:53 +1000

    feat: add MQTT repeater support, web config panel, and EastMesh-specific firmware workflows

---
+++ src/helpers/mqtt/WebPanelCert.h
@@ -0,0 +1,25 @@
+#pragma once
+
+namespace mqtt_web_panel_cert {
+
+static const char kServerCertPem[] =
+    "-----BEGIN CERTIFICATE-----\n"
+    "MIIBiDCCAS2gAwIBAgIUYPagAbDjmNMHx+sqAC5qvOnlywIwCgYIKoZIzj0EAwIw\n"
+    "GTEXMBUGA1UEAwwObWVzaGNvcmUtbG9jYWwwHhcNMjYwNDA5MDE0MjAzWhcNMzYw\n"
+    "NDA2MDE0MjAzWjAZMRcwFQYDVQQDDA5tZXNoY29yZS1sb2NhbDBZMBMGByqGSM49\n"
+    "AgEGCCqGSM49AwEHA0IABJWLgQ/4sDJKykqR9a5brDLR9RikwoZ/6n1Od38rbfzV\n"
+    "ykP2LJIdrxU6zS1gzLk33klycB7AWFL1W7klStdiSpmjUzBRMB0GA1UdDgQWBBSk\n"
+    "dpP2r+UPriHT+hXLiyFdjkFgmTAfBgNVHSMEGDAWgBSkdpP2r+UPriHT+hXLiyFd\n"
+    "jkFgmTAPBgNVHRMBAf8EBTADAQH/MAoGCCqGSM49BAMCA0kAMEYCIQCWS5ZRbON4\n"
+    "71RrwcogEzGuQTNdkbtkRc+517nZ63ZOawIhAPwSy/0yMyhqAoOMpVnhIb+w995f\n"
+    "zH4nApL2Q2U0KI2l\n"
+    "-----END CERTIFICATE-----\n";
+
+static const char kServerKeyPem[] =
+    "-----BEGIN EC PRIVATE KEY-----\n"
+    "MHcCAQEEIGX71nLSiA8qUJXuiZRNzaGizNbMqgOxALSPd/HzfzWAoAoGCCqGSM49\n"
+    "AwEHoUQDQgAElYuBD/iwMkrKSpH1rlusMtH1GKTChn/qfU53fytt/NXKQ/Yskh2v\n"
+    "FTrNLWDMuTfeSXJwHsBYUvVbuSVK12JKmQ==\n"
+    "-----END EC PRIVATE KEY-----\n";
+
+}
